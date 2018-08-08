4ccd66ffb86eff13042ff35840eaa568d403e609
soo <to6sue@naver.com>
2018-08-08 04:47:23 +0000

    beta

---
+++ src/main/java/project/TestApp.java
@@ -0,0 +1,15 @@
+package project;
+
+public class TestApp {
+    private String name;
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+}
