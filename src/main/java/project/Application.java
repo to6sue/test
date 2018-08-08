fa33a8cfa570a5b9a022d7583630b1b17c365ca9
soo <to6sue@naver.com>
2018-08-08 04:28:36 +0000

    first commit

---
+++ src/main/java/project/Application.java
@@ -0,0 +1,12 @@
+package project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello SpringBoot");
+        SpringApplication.run(Application.class, args);
+    }
+}
